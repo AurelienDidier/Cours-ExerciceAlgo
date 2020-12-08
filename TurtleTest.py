c8eef2d78ea406149d388ce6fac585034a6035ee
AurelienDidier <aurelien.didier51@gmail.com>
2020-12-08 03:02:26 +0100

    Create TurtleTest.py

---
+++ TurtleTest.py
@@ -0,0 +1,16 @@
+import turtle, random
+
+def turn(man):
+    if random.randint(0,1)==0:
+        man.left(90)
+    else:
+        man.right(90)
+
+man=turtle.Turtle()
+man.speed(1)
+turtle.delay(0)
+man.forward(5)
+while True:
+    turn(man)
+    man.forward(5)
+
