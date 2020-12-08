da497732da02a65b2143a7b3bbafa79accf9aa77
AurelienDidier <aurelien.didier51@gmail.com>
2020-12-08 03:55:45 +0100

    Update TurtleTest.py

--- TurtleTest.py
+++ TurtleTest.py
@@ -17,3 +17,31 @@ while True:
 """def turn(man):
     man.left(random.randint(0,360))
 """
+
+""" A plusieurs
+import turtle, random
+
+def turn(man):
+    var= random.randint(0,2)
+    if var==0:
+        man.left(90)
+    if var==2:
+        man.right(90)
+
+def addNewRandomWalk(list):
+    man=turtle.Turtle()
+    man.speed(1)
+    man.color(random.random(),random.random(),random.random())
+    return list.append(man)
+
+list=[]
+turtle.delay(0)
+
+for i in range(4):
+    addNewRandomWalk(list)
+
+while True:
+    for i in list:
+        turn(i)
+        i.forward(5)
+"""
