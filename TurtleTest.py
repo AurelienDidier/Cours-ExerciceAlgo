068607a295661eb70565a71ab021e7838af454b2
AurelienDidier <aurelien.didier51@gmail.com>
2020-12-15 08:07:46 +0100

    Update TurtleTest.py

--- TurtleTest.py
+++ TurtleTest.py
@@ -1,53 +1,33 @@
-"""
-import turtle, random
-
-def turn(man):
-    if random.randint(0,1)==0:
-        man.left(90)
-    else:
-        man.right(90)
-
-man=turtle.Turtle()
-man.speed(1)
-turtle.delay(0)
-man.forward(5)
-while True:
-    turn(man)
-    man.forward(5)
-"""
-
-
-
 import turtle,random,math
 
 turtle.delay(0)
 
-def createTortues():
+def createTortues(n):
     tortues=[]
-    for i in range(5):
+    for i in range(n):
         maTortue=turtle.Turtle()
         maTortue.speed(0.1)
         maTortue.color(random.random(),random.random(),random.random())
         tortues.append(maTortue)
     return tortues
 
-def tourne(tortue):
+def tourne(tortue, angle):
     var=random.randint(0,2)
     if var==0:
-        tortue.left(10)
+        tortue.left(angle)
     if var==2:
-        tortue.right(10)
+        tortue.right(angle)
 
 def printDistance(a):
     print(math.sqrt(a.xcor()*a.xcor()+a.ycor()*a.ycor()))
 
-tortues=createTortues()
+tortues=createTortues(5)
 count=0
 while True:
     for i in tortues:
         count+=1
-        tourne(i)
-        i.forward(1)
+        tourne(i,90)
+        i.forward(5)
         if (count==10):
             printDistance(i)
         if (count==50):
